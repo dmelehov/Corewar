2c322cc0dc2ba2f03bed978923cf2bcffa72be18
dmelehov <dmelehov1986@gmail.com>
2018-02-08 14:37:08 +0200

    refresh

--- includes/vm_structs.h
+++ includes/vm_structs.h
@@ -6,14 +6,14 @@
 /*   By: dmelehov <dmelehov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/07 14:51:36 by dmelehov          #+#    #+#             */
-/*   Updated: 2018/02/08 14:06:41 by dmelehov         ###   ########.fr       */
+/*   Updated: 2018/02/08 14:36:50 by dmelehov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VM_STRUCTS_H
 # define VM_STRUCTS_H
 
-# define FLAGS_INIT (t_flags){0,0,0,0,0,0}
+# define FLAGS_INIT (t_flags){0,-1,-1,-1,0,0}
 
 typedef struct		s_players
 {
